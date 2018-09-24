34a2048be39bc50e91a0cbe13157bbdae556aead
Franklin Bado <fbado66@gmail.com>
2018-09-24 18:27:38 -0400

    second push

--- models.rb
+++ models.rb
@@ -1,7 +1,13 @@
 require 'sinatra/activerecord'
 require 'pg'
 
+configure :development do
 set :database, 'postgresql:tumblr'
+end 
+
+configure :production do
+  set :database, ENV["DATABASE_URL"]
+end 
 
 
 class User < ActiveRecord::Base
